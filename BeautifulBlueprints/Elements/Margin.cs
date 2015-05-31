820380ade3d65e7a825b210963dcab57cbcf0313
Martin Evans <martindevans@gmail.com>
2015-05-31 17:38:35 +0100

    Added margin element and size structure
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ BeautifulBlueprints/Elements/Margin.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel;
+using BeautifulBlueprints.Layout;
+using System;
+using System.Collections.Generic;
+
+namespace BeautifulBlueprints.Elements
+{
+    public class Margin
+        : BaseContainerElement
+    {
+        public Size Left { get; private set; }
+        public Size Top { get; private set; }
+        public Size Right { get; private set; }
+        public Size Bottom { get; private set; }
+
+        public Margin(
+            Size? left = null,
+            Size? top = null,
+            Size? right = null,
+            Size? bottom = null,
+            string name = null,
+            decimal minWidth = DEFAULT_MIN_WIDTH,
+            decimal? preferredWidth = null,
+            decimal maxWidth = DEFAULT_MAX_WIDTH,
+            decimal minHeight = DEFAULT_MIN_HEIGHT,
+            decimal? preferredHeight = null,
+            decimal maxHeight = DEFAULT_MAX_HEIGHT
+        )
+            : base(name, minWidth, preferredWidth, maxWidth, minHeight, preferredHeight, maxHeight)
+
+        {
+            Left = left ?? new Size();
+            Top = top ?? new Size();
+            Right = right ?? new Size();
+            Bottom = bottom ?? new Size();
+        }
+
+        internal override IEnumerable<Solver.Solution> Solve(decimal left, decimal right, decimal top, decimal bottom)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override BaseElementContainer Wrap()
+        {
+            return new MarginContainer(this);
+        }
+
+        protected override int MaximumChildren
+        {
+            get
+            {
+                return 1;
+            }
+        }
+    }
+
+    internal class MarginContainer
+        : BaseContainerElement.BaseContainerElementContainer
+    {
+        [DefaultValue(null)]
+        public Size? Left { get; private set; }
+
+        [DefaultValue(null)]
+        public Size? Top { get; private set; }
+
+        [DefaultValue(null)]
+        public Size? Right { get; private set; }
+
+        [DefaultValue(null)]
+        public Size? Bottom { get; private set; }
+
+        public MarginContainer()
+        {
+        }
+
+        public MarginContainer(Margin margin)
+            : base(margin)
+        {
+            Left = margin.Left.AsNullable();
+            Top = margin.Top.AsNullable();
+            Right = margin.Right.AsNullable();
+            Bottom = margin.Bottom.AsNullable();
+        }
+
+        public override BaseElement Unwrap()
+        {
+            return new Margin(Left, Top, Right, Bottom,
+                name: Name,
+                minWidth: MinWidth,
+                preferredWidth: PreferredWidth,
+                maxWidth: MaxWidth,
+                minHeight: MinHeight,
+                preferredHeight: PreferredHeight,
+                maxHeight: MaxHeight
+            );
+        }
+    }
+}
---
+++ BeautifulBlueprints/Size.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace BeautifulBlueprints
+{
+    public struct Size
+    {
+        private const decimal VERY_LARGE_VALUE = 10000;
+
+        public decimal Min { get; private set; }
+        public decimal Prefer { get; private set; }
+        public decimal Max { get; private set; }
+
+        public Size(decimal? min = null, decimal? prefer = null, decimal? max = null)
+            : this()
+        {
+            //Defualt min to zero
+            Min = min ?? 0;
+
+            //Default prefer to smallest size possible
+            Prefer = prefer ?? Math.Min(0, Min);
+
+            //Default to a very large value
+            Max = max ?? Math.Max(Math.Max(VERY_LARGE_VALUE, Prefer), Min);
+        }
+
+        internal Size? AsNullable()
+        {
+            if (Min == 0 && Prefer == 0 && Max == VERY_LARGE_VALUE)
+                return null;
+            return this;
+        }
+    }
+}
