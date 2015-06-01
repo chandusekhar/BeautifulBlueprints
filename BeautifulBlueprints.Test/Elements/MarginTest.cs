437f083fe70b0d532bb792ae87f1fde472c3dd8c
Martin Evans <martindevans@gmail.com>
2015-06-01 01:08:07 +0100

    Implemented layout for margin element
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ BeautifulBlueprints.Test/Elements/MarginTest.cs
@@ -0,0 +1,94 @@
+﻿using BeautifulBlueprints.Elements;
+using BeautifulBlueprints.Layout;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace BeautifulBlueprints.Test.Elements
+{
+    [TestClass]
+    public class MarginTest
+    {
+        [TestMethod]
+        public void AssertThat_MarginElement_ExpandsToFillSpace()
+        {
+            var solution = Solver.Solve(0, 100, 100, 0, new Margin());
+
+            var sol = solution.Single();
+
+            var width = sol.Right - sol.Left;
+            var height = sol.Top - sol.Bottom;
+
+            var ratio = width / height;
+            Assert.AreEqual(1, ratio);
+        }
+
+        [TestMethod]
+        public void AssertThat_MarginElement_ExpandsToFillSpace_WithMargins()
+        {
+            var solution = Solver.Solve(0, 100, 100, 0, new Margin(
+                left: new Size(1, 2, 3),
+                right: new Size(2, 3, 4),
+                top: new Size(3, 4, 5)
+            ) {
+                new Space()
+            }).ToArray();
+
+            Assert.AreEqual(2, solution.Length);
+
+            var sol = solution[1];
+
+            var marginLeft = sol.Left;
+            Assert.IsTrue(marginLeft >= 1 && marginLeft <= 2);
+
+            var marginRight = 100 - sol.Right;
+            Assert.IsTrue(marginRight >= 2 && marginRight <= 4);
+
+            var marginTop = 100 - sol.Top;
+            Assert.IsTrue(marginTop >= 3 && marginTop <= 5);
+
+            var marginBottom = sol.Bottom;
+            Assert.IsTrue(marginBottom == 0);
+        }
+
+        [TestMethod]
+        public void AssertThat_MarginElement_MakesChildAsLargeAsPossible_WhenSizeIsLessThanPreferred()
+        {
+            var solution = Solver.Solve(0, 100, 100, 0, new Margin(
+                left: new Size(1, 2, 3),
+                right: new Size(2, 3, 4),
+                top: new Size(3, 4, 5)
+            ) {
+                new Space(preferredWidth: 200, preferredHeight: 200)
+            }).ToArray();
+
+            Assert.AreEqual(2, solution.Length);
+
+            var sol = solution[1];
+            Assert.AreEqual(1, sol.Left);
+            Assert.AreEqual(98, sol.Right);
+            Assert.AreEqual(97, sol.Top);
+            Assert.AreEqual(0, sol.Bottom);
+        }
+
+        [TestMethod]
+        public void AssertThat_MarginELement_StretchesMarginsOverPreferred_WhenChildMaxIsSmall()
+        {
+            var solution = Solver.Solve(0, 100, 100, 0, new Margin(
+                left: new Size(1, 1, 40),
+                right: new Size(1, 1, 40),
+                top: new Size(1, 1, 40),
+                bottom: new Size(1, 1, 40)
+            ) {
+                new Space(maxWidth: 50, maxHeight: 40)
+            }).ToArray();
+
+            Assert.AreEqual(2, solution.Length);
+
+            var sol = solution[1];
+            Assert.AreEqual(25, sol.Left);
+            Assert.AreEqual(25, 100 - sol.Right);
+            Assert.AreEqual(30, 100 - sol.Top);
+            Assert.AreEqual(30, sol.Bottom);
+        }
+    }
+}
--- BeautifulBlueprints/Elements/Margin.cs
+++ BeautifulBlueprints/Elements/Margin.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel;
-using BeautifulBlueprints.Layout;
-using System;
+﻿using BeautifulBlueprints.Layout;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace BeautifulBlueprints.Elements
 {
@@ -37,7 +37,118 @@ namespace BeautifulBlueprints.Elements
 
         internal override IEnumerable<Solver.Solution> Solve(decimal left, decimal right, decimal top, decimal bottom)
         {
-            throw new NotImplementedException();
+            var solutions = new List<Solver.Solution>();
+
+            var self = FillSpace(left, right, top, bottom);
+            solutions.Add(self);
+
+            if (ChildCount == 0)
+                return solutions;
+
+            var child = Children.Single();
+
+            //Calculate the sizes of the 4 margins
+            decimal l, r, t, b;
+            Resize(out l, out r, Left, Right, self.Right - self.Left, child.MinWidth, child.PreferredWidth, child.MaxWidth);
+            Resize(out t, out b, Top, Bottom, self.Top - self.Bottom, child.MinHeight, child.PreferredHeight, child.MaxHeight);
+
+            //Size the child within the calculated margins
+            solutions.AddRange(child.Solve(left + l, right - r, top - t, bottom + b));
+            return solutions;
+        }
+
+        /// <summary>
+        /// Calculate the size of the margins. The space in the middle (with min/max/preferred size) must fit between the margins
+        /// </summary>
+        /// <param name="margin1"></param>
+        /// <param name="margin2"></param>
+        /// <param name="m1Size"></param>
+        /// <param name="m2Size"></param>
+        /// <param name="extent"></param>
+        /// <param name="childMin"></param>
+        /// <param name="childPrefer"></param>
+        /// <param name="childMax"></param>
+        private void Resize(out decimal margin1, out decimal margin2, Size m1Size, Size m2Size, decimal extent, decimal childMin, decimal childPrefer, decimal childMax)
+        {
+            margin1 = m1Size.Min;
+            margin2 = m2Size.Min;
+            var child = childMin;
+
+            //spare amount of space to distribute to things
+            var spareSize = extent - (m1Size.Min + m2Size.Min + childMin);
+            if (spareSize < 0)
+                throw new LayoutFailureException("Space available is too small for the minimum possible size of the margins and the child element", this);
+
+            //Bail out if we can't possibly expand up to the size available
+            if (childMax + m1Size.Max + m2Size.Max < extent)
+                throw new LayoutFailureException("Space available is too big for the maximum possible size of the margins and the child element", this);
+
+            //Try to expand the child to it's preferred size
+            var spaceToExpandChildToPreferred = childPrefer - child;
+            if (spareSize - spaceToExpandChildToPreferred < 0)
+            {
+                //Consume all spare space expanding the child up towards it's preferred size
+                child += spareSize;
+                return;
+            }
+
+            //We have the space, so expand the child all the way up to it's preferred size
+            child += spaceToExpandChildToPreferred;
+            spareSize -= spaceToExpandChildToPreferred;
+
+            //Now expand the margins up to their preferred size
+            var spaceToExpandMarginsToPreferred = (m1Size.Prefer + m2Size.Prefer) - (margin1 + margin2);
+            var prefRatio = m1Size.Prefer / (m1Size.Prefer + m2Size.Prefer);
+            if (spareSize - spaceToExpandMarginsToPreferred < 0)
+            {
+                //Consume all spare space expanding margins up towards preferred (in ratio of preferences)
+                margin1 += spareSize * prefRatio;
+                margin2 += spareSize * (1 - prefRatio);
+                return;
+            }
+
+            //Expand margins to their preferred size
+            spareSize -= (m1Size.Prefer - margin1);
+            margin1 = m1Size.Prefer;
+            spareSize -= (m2Size.Prefer - margin2);
+            margin2 = m2Size.Prefer;
+
+            //If the space is less than the max of the child we're ok!
+            if (spareSize < childMax)
+                return;
+            else
+                spareSize -= (childMax - child);
+
+            //Expand margins (with remaining space, in ratio of preferred sizes
+            var s1 = spareSize * prefRatio;
+            var s2 = spareSize * (1 - prefRatio);
+            spareSize -= ExpandMargin(ref margin1, m1Size, s1);
+            spareSize -= ExpandMargin(ref margin2, m2Size, s2);
+
+            //If there's *still* spare space, one of the margins must have hit it's max size, add all the space to the other one
+            if (margin1 == m1Size.Max)
+                spareSize -= ExpandMargin(ref margin2, m2Size, spareSize);
+            else
+                spareSize -= ExpandMargin(ref margin1, m1Size, spareSize);
+
+            //if there's *still* spare size we're doomed
+            if (spareSize > 0)
+                throw new LayoutFailureException("Still spare size to allocate even after margins have reached maximum size", this);
+        }
+
+        private static decimal ExpandMargin(ref decimal margin, Size size, decimal amount)
+        {
+            if (margin + amount > size.Max)
+            {
+                var used = (size.Max - margin);
+                margin = size.Max;
+                return used;
+            }
+            else
+            {
+                margin += amount;
+                return amount;
+            }
         }
 
         internal override BaseElementContainer Wrap()
